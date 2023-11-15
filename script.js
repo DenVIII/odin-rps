c8a933d8c27bebfaa30b81b06bef05a852a84b44
Denis <Denis226@gmail.com>
2023-11-15 19:53:58 +0300

    Add playRound function
    playRound function recieves two attributes - playerChoice and
    computerChoice and returns a string with text 'Tie!', 'Player Wins!'
    or 'Computer Wins!'

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ function getComputerChoice() {
     return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+               (playerSelection === 'paper' && computerSelection === 'scissors') ||
+               (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return 'Player Wins!';
+    } else {
+        return 'Computer Wins!';
+    }
+}
+
+const playerChoice = 'rOcK';
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice, computerChoice));
